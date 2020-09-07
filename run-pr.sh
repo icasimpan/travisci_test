e11ed648714bbc28cc13afbd10cf569d4f24a439
Ismael Casimpan <ismael@prometsource.com>
2020-09-08 05:28:53 +0800

    Update

--- run-pr.sh
+++ run-pr.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+set -xv
+
 curl -X POST -H -u "icasimpan:${GH_TOKEN}" "Accept: application/vnd.github.v3+json" https://api.github.com/repos/icasimpan/travisci_test/pulls -d '{"title":"testing1","head":"modified_code","base":"master"}'
